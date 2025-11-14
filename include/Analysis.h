4805a8b8955f9e5ea6de00198f5c60ffc268be48
Anh Quan <nguyenanhquan180606@gmail.com>
2025-11-15 01:27:42 +0700

    Adding more statistical strategy to analyze

--- include/Analysis.h
+++ include/Analysis.h
@@ -22,18 +22,29 @@ using namespace std;
  */
 class Analysis {
 private:
-    const StatTrie& trie; // Trie được phân tích
+    
+    const StatTrie& trie;       // Trie được phân tích
+    double freqThreshold;       // ngưỡng hiếm
+    double avrEntropy, avrDepth, expectLen;             // call computeAverages() to compute these
+    double stdDeviEntropy, stdDeviDepth, stdDeviLen;    // call computeStandardDeviations() to compute these
+    int depthZscore, lengthZ_score, entropyZscore;      // z score thresholds
 
     // Helpers
     double totalWords() const;
     void writeHeader(ofstream& fout) const;
 
-public:
-    friend class StatTrie;
-    explicit Analysis(const StatTrie& trie);
+    void computeAverages();
+    void computeStandardDeviations();
 
-    double computeAverageDepth() const;
-    double computeAverageBranchingFactor() const;
+public:
+
+    explicit Analysis(const StatTrie& trie, double freq = 0.05, double expect_len_z = 3, double entropy_z = 3, double avr_dep_z = 3);
+
+    void analyze();
+    double Z_score (vector<double> elements);
+    double entropyOf(const StatTrie::Node* node);
+   
+    // double computeAverageBranchingFactor() const;
     double computeAnomalyRatio() const;
 
     // ===== Detection =====
--- src/Analysis.cpp
+++ src/Analysis.cpp
@@ -1,124 +1,170 @@
 #include "../include/Analysis.h"
 
 /* ===================== Constructor ===================== */
-Analysis::Analysis(const StatTrie& trie) : trie(trie) {}
+// Analysis::Analysis(const StatTrie& trie) : trie(trie) {}
+Analysis::Analysis(const StatTrie& trie, double freq, double expect_len_z, double entropy_z, double avr_dep_z) 
+    : trie(trie), freqThreshold(freq), lengthZ_score(expect_len_z),
+    entropyZscore(entropy_z), depthZscore(avr_dep_z) {}
+
+
+
+
+
+
+void Analysis::computeAverages() {
+
+    double totalEntropy = 0, total_depth = 0, total_len = 0;
+    auto collector = [&] (const StatTrie::Node* node, const string &prefix) {
+        if (node->isEnd) {
+            total_depth += prefix.size();
+            total_len += (node->count) * prefix.size();
+        }
+        totalEntropy += entropyOf(node);
+    };
+    trie.traverse(collector);
+
+    avrEntropy = totalEntropy / trie.totalNodes();
+    avrDepth = total_depth / trie.totalUniqueWords();
+    expectLen = total_len / trie.totalInsertedWords();
+
+}
+
+void Analysis::analyze() {
+    
+}
+
+
 
 /* ===================== Helpers ===================== */
-double Analysis::totalWords() const {
-    return (double)trie.totalInsertedWords();
-}
+// double Analysis::totalWords() const {
+//     return (double)trie.totalInsertedWords();
+// }
 
-void Analysis::writeHeader(ofstream& fout) const {
-    fout << "=== Trie Statistical Analysis Report ===\n";
-    fout << "Total Nodes: " << trie.totalNodes() << "\n";
-    fout << "Unique Sequences: " << trie.totalUniqueWords() << "\n";
-    fout << "Inserted Sequences: " << trie.totalInsertedWords() << "\n";
-    fout << "Inserted Chars:" << trie.totalInsertedCharacters() << "\n";
-    fout << "Anomaly Threshold: " << trie.getAnomalyRate() << "\n\n";
-}
+// void Analysis::writeHeader(ofstream& fout) const {
+//     fout << "=== Trie Statistical Analysis Report ===\n";
+//     fout << "Total Nodes: " << trie.totalNodes() << "\n";
+//     fout << "Unique Sequences: " << trie.totalUniqueWords() << "\n";
+//     fout << "Inserted Sequences: " << trie.totalInsertedWords() << "\n";
+//     fout << "Inserted Chars:" << trie.totalInsertedCharacters() << "\n";
+//     fout << "Anomaly Threshold: " << trie.getAnomalyRate() << "\n\n";
+// }
 
 
-double Analysis::computeAverageDepth() const {
-    double totalDepth = 0.0;
-    double totalWordsCounted = 0.0;
 
-    trie.traverse([&](const StatTrie::Node* node, const string& prefix) {
-        if (node->isEnd) {
-            totalDepth += prefix.size();
-            totalWordsCounted++;
-        }
-    });
-    return totalWordsCounted > 0 ? totalDepth / totalWordsCounted : 0.0;
-}
 
-double Analysis::computeAverageBranchingFactor() const {
-    double totalBranches = 0.0;
-    double totalNodes = 0.0;
 
-    trie.traverse([&](const StatTrie::Node* node, const string&) {
-        totalBranches += node->children.size();
-        totalNodes++;
-    });
-    return totalNodes > 0 ? totalBranches / totalNodes : 0.0;
-}
+
+
+
+/* ==================== Structure-based analysis ==================== */
+
+// double Analysis::computeAverageDepth() const {
+//     double totalDepth = 0.0;
+//     double totalWordsCounted = 0.0;
+
+//     trie.traverse([&](const StatTrie::Node* node, const string& prefix) {
+//         if (node->isEnd) {
+//             totalDepth += prefix.size();
+//             totalWordsCounted++;
+//         }
+//     });
+//     return totalWordsCounted > 0 ? totalDepth / totalWordsCounted : 0.0;
+// }
+
+// double Analysis::computeAverageBranchingFactor() const {
+//     double totalBranches = 0.0;
+//     double totalNodes = 0.0;
+
+//     trie.traverse([&](const StatTrie::Node* node, const string&) {
+//         totalBranches += node->children.size();
+//         totalNodes++;
+//     });
+//     return totalNodes > 0 ? totalBranches / totalNodes : 0.0;
+// }
 
 // Tính tỷ lệ chuỗi bất thường trên tổng chuỗi (dựa vào tần suất)
-double Analysis::computeAnomalyRatio() const {
-    double total = totalWords();
-    if (total == 0) return 0.0;
+// double Analysis::computeAnomalyRatio() const {
+//     double total = totalWords();
+//     if (total == 0) return 0.0;
 
-    int anomalyCount = 0, uniqueCount = 0;
+//     int anomalyCount = 0, uniqueCount = 0;
 
-    trie.traverse([&](const StatTrie::Node* node, const string&) {
-        if (node->isEnd) {
-            uniqueCount++;
-            if ((double)(node->count / total) < trie.getAnomalyRate())
-                anomalyCount++;
-        }
-    });
+//     trie.traverse([&](const StatTrie::Node* node, const string&) {
+//         if (node->isEnd) {
+//             uniqueCount++;
+//             if ((double)(node->count / total) < trie.getAnomalyRate())
+//                 anomalyCount++;
+//         }
+//     });
 
-    return uniqueCount > 0 ? (double)anomalyCount / uniqueCount : 0.0;
-}
+//     return uniqueCount > 0 ? (double)anomalyCount / uniqueCount : 0.0;
+// }
+
+
+/* ==================== Data-based analysis ==================== */
 
 //Lưu các chuỗi bất thường (dựa trên tần suất/độ sâu/độ thưa thớt nhánh của node)
-vector<string> Analysis::detectAnomalies() const {
-    vector<string> anomalies;
+// vector<string> Analysis::detectAnomalies() const {
+//     vector<string> anomalies;
 
-    double total = totalWords();
-    if (total == 0) return anomalies;
+//     double total = totalWords();
+//     if (total == 0) return anomalies;
 
-    double avgDepth = computeAverageDepth();
-    double avgBranch = computeAverageBranchingFactor();
+//     double avgDepth = computeAverageDepth();
+//     double avgBranch = computeAverageBranchingFactor();
 
-    trie.traverse([&](const StatTrie::Node* node, const string& prefix) {
-        if (node->isEnd) {
-            double freq = node->count / total;
-            bool rare = freq < trie.getAnomalyRate();
-            bool deep = prefix.size() > avgDepth * 2.5;
-            bool sparse = node->children.size() < avgBranch / 2.0;
-            bool shallow = prefix.size() < avgDepth * 0.5;
+//     trie.traverse([&](const StatTrie::Node* node, const string& prefix) {
+//         if (node->isEnd) {
+//             double freq = node->count / total;
+//             bool rare = freq < trie.getAnomalyRate();
+//             bool deep = prefix.size() > avgDepth * 2.5;
+//             bool sparse = node->children.size() < avgBranch / 2.0;
+//             bool shallow = prefix.size() < avgDepth * 0.5;
 
-            /*if (rare || deep || sparse)*/   if(rare)
-                anomalies.push_back(prefix);
-        }
-    });
+//             /*if (rare || deep || sparse)*/   if(rare)
+//                 anomalies.push_back(prefix);
+//         }
+//     });
 
-    return anomalies;
-}
+//     return anomalies;
+// }
+
 
 /* ===================== Generate Report ===================== */
-void Analysis::generateReport(const string& filename) const {
-    ofstream fout(filename);
-    if (!fout.is_open()) {
-        cerr << "[ERROR] Cannot open report file: " << filename << endl;
-        return;
-    }
+// void Analysis::generateReport(const string& filename) const {
+//     ofstream fout(filename);
+//     if (!fout.is_open()) {
+//         cerr << "[ERROR] Cannot open report file: " << filename << endl;
+//         return;
+//     }
 
-    fout << fixed << setprecision(4);
-    writeHeader(fout);
+//     fout << fixed << setprecision(4);
+//     writeHeader(fout);
 
-    double avgDepth = computeAverageDepth();
-    double avgBranch = computeAverageBranchingFactor();
-    double anomalyRatio = computeAnomalyRatio();
+//     double avgDepth = computeAverageDepth();
+//     double avgBranch = computeAverageBranchingFactor();
+//     double anomalyRatio = computeAnomalyRatio();
 
-    fout << "--- Statistical Summary ---\n";
-    fout << "Average Depth: " << avgDepth << "\n";
-    fout << "Average Branching Factor: " << avgBranch << "\n";
-    fout << "Anomaly Ratio: " << anomalyRatio * 100 << "%\n\n";
+//     fout << "--- Statistical Summary ---\n";
+//     fout << "Average Depth: " << avgDepth << "\n";
+//     fout << "Average Branching Factor: " << avgBranch << "\n";
+//     fout << "Anomaly Ratio: " << anomalyRatio * 100 << "%\n\n";
 
-    auto anomalies = detectAnomalies();
-    fout << "--- Detected Anomalies ---\n";
-    fout << "Count: " << anomalies.size() << "\n";
-    fout << "Ratio: " << (double)anomalies.size() / trie.totalUniqueWords() * 100 << "%\n\n";
-    fout << "Examples:\n";
-    for (size_t i = 0; i < anomalies.size(); ++i)
-        fout << anomalies[i] << "\n";
+//     auto anomalies = detectAnomalies();
+//     fout << "--- Detected Anomalies ---\n";
+//     fout << "Count: " << anomalies.size() << "\n";
+//     fout << "Ratio: " << (double)anomalies.size() / trie.totalUniqueWords() * 100 << "%\n\n";
+//     fout << "Examples:\n";
+//     for (size_t i = 0; i < anomalies.size(); ++i)
+//         fout << anomalies[i] << "\n";
 
-    fout.close();
-    cout << "[INFO] Analysis report saved to " << filename << endl;
-}
+//     fout.close();
+//     cout << "[INFO] Analysis report saved to " << filename << endl;
+// }
+
 
 /* ===================== Export JSON of trie ===================== */
+
 void Analysis::exportJSON (const string exportPath) {
 
     ofstream file (exportPath, ios::trunc);
